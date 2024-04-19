c1d41bd6d73ee554c136c1829971f6879bd5ea0f
tobias-bonsack <tobias.bonsack@gmail.com>
2024-04-19 20:29:25 +0200

    enum knows class for later cast in resolve

--- de.tonsias.basis.model/src/de/tonsias/basis/model/enums/SingleValueTypes.java
+++ de.tonsias.basis.model/src/de/tonsias/basis/model/enums/SingleValueTypes.java
@@ -1,6 +1,19 @@
 package de.tonsias.basis.model.enums;
 
+import de.tonsias.basis.model.impl.value.SingleStringValue;
+import de.tonsias.basis.model.interfaces.ISingleValue;
+
 public enum SingleValueTypes {
 
-	SINGLE_STRING;
+	SINGLE_STRING(SingleStringValue.class);
+
+	private final Class<? extends ISingleValue<?>> _clazz;
+
+	SingleValueTypes(Class<? extends ISingleValue<?>> clazz) {
+		_clazz = clazz;
+	}
+
+	public Class<? extends ISingleValue<?>> getClazz() {
+		return _clazz;
+	}
 }
