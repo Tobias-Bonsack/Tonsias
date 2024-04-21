2d1f75e07116a40062d308c2fa6abc6c430e0993
tobias-bonsack <tobias.bonsack@gmail.com>
2024-04-21 17:52:12 +0200

    min ui button. next step is to impl instanzserviceimpl to create/get root element

--- de.tonsias.basis.ui/src/de/tonsias/basis/ui/test.java
+++ de.tonsias.basis.ui/src/de/tonsias/basis/ui/test.java
@@ -1,6 +1,24 @@
 package de.tonsias.basis.ui;
 
-public class test {
+import org.eclipse.jface.widgets.ButtonFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 
+import de.tonsias.basis.osgi.intf.IInstanzService;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+
+public class test {
 	
+	@Inject
+	IInstanzService _instanzService;
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		ButtonFactory.newButton(SWT.None).text("button").onSelect(e -> createRoot()).create(parent);
+	}
+
+	private void createRoot() {
+	}
+
 }
