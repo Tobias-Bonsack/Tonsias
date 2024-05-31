8c068d6bf12155fe1160d5964fdbaec1c8225feb
tobias-bonsack <tobias.bonsack@gmail.com>
2024-05-31 12:11:55 +0200

    add dependecys for tests (mockito)

---
+++ de.tonsias.basis.osgi.test/src/de/tonsias/basis/osgi/test/impl/KeyServiceImplTest.java
@@ -0,0 +1,17 @@
+package de.tonsias.basis.osgi.test.impl;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import de.tonsias.basis.osgi.impl.KeyServiceImpl;
+
+public class KeyServiceImplTest {
+
+	@Mock
+	KeyServiceImpl _keyService;
+
+	@Test
+	void test() {
+	}
+
+}
