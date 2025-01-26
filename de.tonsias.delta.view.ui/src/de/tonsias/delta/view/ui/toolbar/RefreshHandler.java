b0d684a24019ae204116e4181aee987c2447b42b
tobias-bonsack <tobias.bonsack@gmail.com>
2025-01-26 14:15:59 +0100

    smaller icon

---
+++ de.tonsias.delta.view.ui/src/de/tonsias/delta/view/ui/toolbar/RefreshHandler.java
@@ -0,0 +1,16 @@
+package de.tonsias.delta.view.ui.toolbar;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class RefreshHandler {
+
+	public RefreshHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Execute
+	public void execute() {
+
+	}
+
+}
